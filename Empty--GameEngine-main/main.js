0b3fd7e6575cc0b351a66157e0be459af9f7b69d
arafam <arafam@uw.edu>
2024-12-28 11:06:38 -0800

    Updated main.js and index.html

--- Empty--GameEngine-main/main.js
+++ Empty--GameEngine-main/main.js
@@ -2,11 +2,16 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+// Loads sprite sheet
+ASSET_MANAGER.queueDownload("./Empty--GameEngine-main sprites/Miko.png");
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
 	gameEngine.init(ctx);
+
+	gameEngine.addEntity(new Miko(gameEngine));
 
 	gameEngine.start();
 });
